c2f3822e0306ae8b4bf183ddf6281d9dd1e09ece
linrk0698 <linrk0698@710e1f5b-ce3c-4422-ba63-7e5fd6020d8d>
2017-01-25 00:21:59 +0000

    add option price loading

--- trunk/JKL_Research/src/priceLoader.py
+++ trunk/JKL_Research/src/priceLoader.py
@@ -9,12 +9,28 @@ import pandas_datareader.data as web
 import matplotlib
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-    # Read S&P 500 Stock Tickers
-    sp500_list = pd.read_csv('list_SP500_test.csv')
-    print (sp500_list['Ticker'])
-    goog = web.DataReader(sp500_list['Ticker'], data_source='google', start='1/1/2016', end='08/07/2016')
-    goog['Close'].replace(to_replace=0,method='ffill')
+from pandas.io.data import Options
+
+# Read S&P 500 Stock Tickers
+tickerFile = '../data/list_SP500_test.csv'
+sp500_list = pd.read_csv(tickerFile)
+print (sp500_list['Ticker'])
+goog = web.DataReader(sp500_list['Ticker'], data_source='google', start='1/1/2016', end='08/07/2016')
+goog['Close'].fillna(0)
+
+spOption_ggle= Options('aapl','yahoo')
+optionData_ggle = spOption_ggle.get_all_data()
+optionData_ggle.iloc[0:5,0:5]
+
+from wallstreet import Stock, Call, Put
+s = Stock('AAPL')
+s.prices
+s.change
+s.last_trade
+g = Call('GOOG',d=12,m=2,y=2016,strike=700)
+g.price
+g.implied_volatility()
+
 #     goog['Log_Ret'] = np.log(goog['Close'] / goog['Close'].shift(1))
 #     goog['Volatility'] = goog['Log_Ret'].rolling(window=252,center=False).std()*np.sqrt(252)
 #     goog['Log_Ret'].to_csv('log_ret.csv')
