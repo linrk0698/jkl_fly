2f976dfb300015e6efe382ce0ac760fff99af18b
linrk0698 <linrk0698@gmail.com>
2017-02-01 22:35:20 +0000

    price loader for index and options

--- JKL_Research/src/priceLoader.py
+++ JKL_Research/src/priceLoader.py
@@ -8,16 +8,56 @@ import pandas as pd
 import pandas_datareader.data as web
 import matplotlib
 import matplotlib.pyplot as plt
+from datetime import date,datetime, timedelta
+from pandas_datareader.data import Options
+import math
 
-from pandas.io.data import Options
+cols = ['Last','Bid','Ask','Vol','Open_Int','IV']
+tickerList = pd.read_csv('../data/list_option_ticker.csv')
+indexSym = tickerList['ticker']
+underlying = tickerList['underlying']
+isIndex  = tickerList['index']
+steps = tickerList['step']
 
-spOption_ggle= Options('aapl','yahoo')
-optionData_ggle = spOption_ggle.get_all_data()
-optionData_ggle.iloc[0:5,0:5]
-#
-#from wallstreet import Stock, Call, Put
-#s = Stock('AAPL')
-#g = Call('GOOG',d=17,m=3,y=2017,strike=700)
-#g.price
-#g.implied_volatility()
+for counter in range (0,len(indexSym)):
+    curIndexSym = indexSym[counter]
+    curUnderlying = underlying[counter]
+    curSteps = float(steps[counter])
+    filename = '../data/optionData.csv'
+    curIsIndex = bool(isIndex[counter])
+    print('Processing '+curIndexSym)
+    if curIsIndex:
+        option= Options('^'+curIndexSym,'yahoo')
+        indexData = web.DataReader('^'+curUnderlying,'yahoo',start,end)
+    else:
+        option = Options(curIndexSym,'yahoo')
+        indexData = web.DataReader(curUnderlying,'yahoo',start,end)
+    optionData = option.get_all_data()
+    optionData = optionData.loc[:,cols]
+    
+    # Select price/index level around which we will save the price. 
+    LEVEL = 0.07
+    today = date.today() - timedelta(1)
+    if today.isoweekday() == 7:
+        start = today-timedelta(2)
+    elif today.isoweekday() == 6:
+        start = today-timedelta(1)
+    else:
+        start = today
+    end = start
+    lowerBnd = max (indexData['Close'][0] - indexData['Close'][0]*0.07,0)
+    upperBnd = indexData['Close'][0]+indexData['Close'][0]*0.07
+    lowerBnd = math.floor(lowerBnd/curSteps)*curSteps
+    upperBnd = math.ceil(upperBnd/curSteps)*curSteps
+    idxx = optionData.index.get_level_values(level='Strike')
+    mask = (idxx>=lowerBnd) & (idxx <=upperBnd)
+    idx = pd.IndexSlice
+    optionData2 = optionData.loc[idx[mask,:,:,:],:]
 
+    # Add another layer to the index. 
+    optionData2['Dates'] = today.strftime('%Y%m%d')
+    optionData2.set_index('Dates',append=True,inplace=True)
+    optionData2=optionData2.reorder_levels(['Dates','Symbol','Strike','Expiry','Type'])
+    # Write out file
+    optionData2.to_csv(filename,mode='a',header=False)
+print('Finished all')
